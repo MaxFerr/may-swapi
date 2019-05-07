95256e157780aa500218f481d5e6b021825d95af
Max <ferromassimo1989@gmail.com>
2019-05-07 20:07:52 +0200

    test3

---
+++ build/precache-manifest.454ec5f043aaf8f8efce9940618f4230.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9f9a5cae236405bddb47b0e6a9affa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321b813b6f043f5f567e",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4d0111665861151b6b21",
+    "url": "/static/css/main.76d2196a.chunk.css"
+  },
+  {
+    "revision": "321b813b6f043f5f567e",
+    "url": "/static/js/2.be8f8b88.chunk.js"
+  },
+  {
+    "revision": "4d0111665861151b6b21",
+    "url": "/static/js/main.033044c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/static/media/Starjhol.9c91297e.ttf"
+  }
+]);
